3ac34a9c2edba7e057e94fa4d538b1d0f6f637dd
Ben Moran <github@benmoran.net>
2017-06-17 22:31:00 +0100

    config.js

--- src/js/src/config.js
+++ src/js/src/config.js
@@ -37,25 +37,25 @@ src/js/src/config.js
 /* exported Config */
 
 var Config = {
-  keen: {
+    keen: {
     projectId: '53a48cb6bcb79c3f5e000000',
     writeKey: 'bcfbf33780ecb04cf8c1f8285688dd0d10018f4041908d209b812326e07f5c3a4e4004452cbbfe051d98d589829f48cf1c1d5e81839d2ac7f40f5c8354ef0617b7b15e85a341c7f424e2b5aac844ba86f3ff22287231120699a428c4db7b149d0376577e5260ad7e54aefd9ec026ff7b'
   },
   api: {
     tube: {
-      status: 'http://pebble.matthewtole.com/uk-transport/tube/status.json',
-      details: 'http://pebble.matthewtole.com/uk-transport/tube/details.json'
+      status: 'http://uktransport.benmoran.net/uk-transport/tube/status.json',
+      details: 'http://uktransport.benmoran.net/uk-transport/tube/details.json'
     },
     train: {
-      stations: 'http://pebble.matthewtole.com/uk-transport/2/train/stations.json',
-      departures: 'http://pebble.matthewtole.com/uk-transport/train/departures.json'
+      stations: 'http://uktransport.benmoran.net/uk-transport/2/train/stations.json',
+      departures: 'http://uktransport.benmoran.net/uk-transport/train/departures.json'
     },
     bus: {
-      stops: 'http://pebble.matthewtole.com/uk-transport/bus/stops.json',
-      departures: 'http://pebble.matthewtole.com/uk-transport/bus/departures.json'
+      stops: 'http://uktransport.benmoran.net/uk-transport/bus/stops.json',
+      departures: 'http://uktransport.benmoran.net/uk-transport/bus/departures.json'
     }
   },
-  debug: false
+  debug: true
 };
 
 module.exports = Config;
